1fff2be2cb62f4e0a496a020263702785590b68d
kajarosz <karolinaannajarosz@gmail.com>
2022-04-28 23:27:28 +0200

    Changes out of range HTML template to universal error template. Implemented error template rendering when 404 occurs.

--- flask_app/main/routes.py
+++ flask_app/main/routes.py
@@ -5,8 +5,11 @@ from main.api_requests import songs_list, InvalidInputException
 def configure_routes(app):
 
     @app.errorhandler(404)
-    def resource_not_found(e):
-        return jsonify(error=str(e)), 404
+    def not_found(e):
+        response = {'error': 'Page not found',
+            'details': str(e)}
+        jsonify(response)
+        return render_template('error.html', title="page", response = response), 404
 
     @app.route('/random_songs/<int:number>', methods=['GET'])
     def random_songs(number):
@@ -18,5 +21,5 @@ def configure_routes(app):
             response = {'error': 'Invalid request',
             'details': 'Please enter intiger from 5 to 20.'}
             jsonify(response)
-            return render_template('out_of_range.html', title="page", response = response)
+            return render_template('error.html', title="page", response = response)
 
